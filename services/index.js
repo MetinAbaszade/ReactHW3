13096d37e992e3ca712663e0daef4207619bbeb0
Metin Abaszade <metin.abaszade@gmail.com>
2023-05-01 10:15:38 +0400

    Remove all files and folders

--- services/index.js
+++
@@ -1,2 +0,0 @@
-export * as PostService from './postservice'
-export * as UserService from './usersevice'
--- services/usersevice.js
+++
@@ -1,5 +0,0 @@
-import { post, get, postJSON } from './requestservice'
-
-export const getUsers = () => get('/users');
-export const getUserById = (id) => get(`/users/${id}`);
-export const addUser = (data) => post(`/users`, data);
